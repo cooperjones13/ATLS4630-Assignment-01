6e234e0c48664088272158c35dd7a89d189eb35d
cooperjones13 <39482154+cooperjones13@users.noreply.github.com>
2025-08-29 12:31:34 -0600

    Code Cleanup and Documentation
    - added project description and link to README
    - Cleaned up and commented in script.js
    - move inline css styling for SVGs into style.css
    - Minor aesthetic changes

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Assignment 01 for **ATLS 4360** - *Front End Web Development* - Fall 2025
 > - Respond to at least two different types of events using event listeners.
 >
 > Both your Javascript and your CSS should be in external files (not embedded in the HTML).
+
+## Assignment Description
+
+For this assignment, I created a dice rolling app for TTRPGs and other board games that require polyhedral dice. I added in some interactivity with some effects on hover and click affecting the content and styling of the page. This app is hosted on Vercel.
+
+### [Link to Project on Vercel](https://atls-4630-assignment-01.vercel.app/)
--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-let imageContainers = document.getElementsByClassName("image-container");
+let diceSizes = [20, 12, 10, 8, 6, 4];
 
-console.log(imageContainers);
-
-Array.from(imageContainers).forEach(container => {
-    container.addEventListener('mouseenter', ()=>{
-        console.log(container)
-        container.style.flexGrow = 2;
-        container.children[2].style.color = "#000000aa"
-    });
-    container.addEventListener('mouseleave', ()=>{
-        console.log(container)
-        container.style.flexGrow = 1;
-        container.children[2].style.color = "transparent"
-    });
-});
-
-let diceSizes = [20, 12, 10, 8, 6, 4]
-
+//looping over each container based on the id naming convention instead of attaching eventListeners each time
 diceSizes.forEach(diceSize => {
     let diceContainer = document.getElementById(`d${diceSize}-container`);
+    //Adds click effects to roll each dice
     diceContainer.addEventListener('click', () =>{
-        diceContainer.children[1].textContent = Math.ceil(Math.random()*diceSize)
-        diceContainer.children[1].style.color = '#51bb51';
+        diceContainer.children[1].textContent = Math.ceil(Math.random()*diceSize) //Generates a random number based on the number of sides on the current die
+        diceContainer.children[1].style.color = '#51bb51'; //temporarily sets color to green and enlarges text to indicate that the dice has been rolled
         diceContainer.children[1].style.fontSize = '3rem';
         setTimeout(()=>{
-            diceContainer.children[1].style.color = 'white';
+            diceContainer.children[1].style.color = 'white'; //sets the color and size back to normal
             diceContainer.children[1].style.fontSize = '2rem';
         },1500)
+    });
+
+    // Adds Hover Effects for each dice container
+    diceContainer.addEventListener('mouseenter', ()=>{
+        console.log(`Mount entered d${diceSize} container`)
+        diceContainer.style.flexGrow = 2; //allows the currently hovered box to grow slightly on hover
+        diceContainer.children[2].style.color = "#000000aa" //Makes the dice description "Twenty Sided Die" visible on hover
+    });
+    diceContainer.addEventListener('mouseleave', ()=>{
+        //sets everything back to normal after hover ends
+        console.log(`Mount left d${diceSize} container`)
+        diceContainer.style.flexGrow = 1;
+        diceContainer.children[2].style.color = "transparent"
     });
 });
